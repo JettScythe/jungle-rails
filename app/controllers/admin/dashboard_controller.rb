d916e126ec325c725ea99924d660d43e1d4600d3
JettScythe <jettscythe@hotmail.com>
2021-06-23 12:56:52 -0300

    implement admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
                                password: ENV["HTTP_BASIC_PASSWORD"],
                                if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
   def show
+    @products = Product.count
+    @category = Category.count
   end
 end
